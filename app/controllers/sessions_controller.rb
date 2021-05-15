f0ec1309994008262b3b3eb26affc99846c7ec68
zahidalik <zahidkhan545@icloud.com>
2021-05-15 11:29:52 +0300

    Create sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def new
+    # code
+  end
+
+  def create
+    # code
+  end
+
+  def destroy
+    # code
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#welcome'
+  namespace :sessions do
+    get 'login' => :new
+    post 'signin' => :create
+    delete 'logout' => :destroy
+  end
 end
